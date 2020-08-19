35cbf19961e11bdde0031afa79c162a7a435d0aa
Ramsay <ramsaygonzalez@outlook.com>
2020-08-19 00:57:16 -0400

    Added the ability to delete objects in the db.json file.  Updated readme.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,7 @@ const { v4: uuidv4 } = require('uuid');
 const dbNotes = require("../db/db.json");
 
 module.exports = function (app) {
-    // Resources https://nodejs.org/api/fs.html || https://stackoverflow.com/questions/56711269/create-new-object-and-write-to-file-with-fs-writefile || https://stackoverflow.com/questions/36856232/write-add-data-in-json-file-using-node-js
+    // Resources https://nodejs.org/api/fs.html 
 
     // GET route 
     app.get('/api/notes', function (req, res) {
@@ -27,14 +27,14 @@ module.exports = function (app) {
 
         fs.readFile('./db/db.json', 'utf8', function (err, data) {
             if (err) {
-                console.log(err)
+                console.log(err);
             }
             notesArr = JSON.parse(data);
-            notesArr.push(note);
+            notesArr.push(notes);
 
             fs.writeFile('./db/db.json', JSON.stringify(notesArr), function (err) {
                 if (err) {
-                    console.log(err)
+                    console.log(err);
                 }
                 return res.json(notesArr);
             });
@@ -42,5 +42,32 @@ module.exports = function (app) {
 
     });
 
-     // Delete route
-};
+    // Resources https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach || https://stackoverflow.com/questions/15287865/remove-array-element-based-on-object-property
+    // Delete route
+    app.delete("/api/notes/:id", function (req, res) {
+        let noteId = req.params.id;
+        fs.readFile('./db/db.json', 'utf8', function (err, data) {
+            if (err) {
+                console.log(err);
+            }
+            const notesArr = JSON.parse(data);
+
+            console.log(notesArr)
+
+            const newNotesArr = notesArr.filter(item => item.id !== noteId);
+
+            console.log("-------------")
+
+            console.log(newNotesArr)
+
+            fs.writeFile('./db/db.json', JSON.stringify(newNotesArr), function (err) {
+                if (err) {
+                    console.log(err)
+                }
+                return res.json(dbNotes);
+            })
+
+        });
+    });
+
+}
